c2bfd360f473f4a0dda4bb03853ed01ed5a68569
Fenz <uukooo@126.com>
2018-03-23 18:31:38 +0800

    Add api method ->call() of Gluer

--- src/Gluer/__object.php
+++ src/Gluer/__object.php
@@ -12,4 +12,29 @@ use Fenzland\DataParser\Transformer;
 trait __object
 {
 	use THoldsProperties;
+
+	/**
+	 * Method call
+	 *
+	 * @access public
+	 *
+	 * @param  array $origin_params
+	 *
+	 * @return array
+	 */
+	public function call( array$origin_params ):array
+	{
+		$params= $this->request_transformer->transform( $origin_params );
+
+		$response= HTTP::url( $this->makeUrl( $params ) )
+			->method( $this->method )
+			->headers( $params['headers']??[] )
+			->header( 'Accept', $this->response_content_type )
+			->header( 'Content-Type', $this->request_content_type )
+			->send( $this->makeBody( $params['body']??null ) )
+		;
+
+		return $this->parseResponse( $response );
+	}
+
 }
