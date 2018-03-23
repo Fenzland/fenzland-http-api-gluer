383b50c0097530601b7a3db1bcd8dea5549de378
Fenz <uukooo@126.com>
2018-03-23 18:18:02 +0800

    Add empty class Gluer

---
+++ src/Gluer.php
@@ -0,0 +1,13 @@
+<?php
+
+declare( strict_types= 1 );
+
+namespace Fenzland\HttpApiGluer;
+
+////////////////////////////////////////////////////////////////
+
+class Gluer
+{
+	use Gluer\__static;
+	use Gluer\__object;
+}
