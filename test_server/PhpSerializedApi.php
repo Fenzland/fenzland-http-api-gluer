62b620a81dc74e47fa66aabbcd98e052fb7a5f41
Fenz <uukooo@126.com>
2018-03-23 20:45:50 +0800

    Add php serialized api and test

---
+++ test_server/PhpSerializedApi.php
@@ -0,0 +1,52 @@
+<?php
+
+declare( strict_types= 1 );
+
+namespace Fenzland\HttpApiGluer\TestServer;
+
+////////////////////////////////////////////////////////////////
+
+class PhpSerializedApi
+{
+
+	/**
+	 * Static method _GET_
+	 *
+	 * @static
+	 * @access public
+	 *
+	 * @return void
+	 */
+	static public function _GET_():void
+	{
+		header( 'HTTP/1.1 200 Not found' );
+		header( 'Content-Type: application/json' );
+		header( 'Gluer-Test: '.$_SERVER['HTTP_GLUER_TEST'] );
+		echo serialize( [
+			'call'=> 'successful',
+			'path'=> $_SERVER['PATHINFO']??strtok( $_SERVER['REQUEST_URI'], '?' ),
+			'query'=> $_GET,
+		] );
+	}
+
+	/**
+	 * Static method _POST_
+	 *
+	 * @static
+	 * @access public
+	 *
+	 * @return void
+	 */
+	static public function _POST_():void
+	{
+		$data= unserialize( file_get_contents( 'php://input' ) );
+
+		header( 'HTTP/1.1 200 Not found' );
+		header( 'Content-Type: application/json' );
+		echo serialize( [
+			'call'=> 'successful',
+			'data'=> $data,
+		] );
+	}
+
+}
